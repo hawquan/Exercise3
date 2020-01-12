805be10ac588e803fbe887967ee61a05ba3fa0b3
hawquan <hawquan@gmail.com>
2020-01-12 19:43:18 +0800

    yes

--- app/src/main/java/com/example/exercise3/MainActivity.kt
+++ app/src/main/java/com/example/exercise3/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.exercise3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.RadioButton
-import android.widget.Spinner
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
